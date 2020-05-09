dff8e5d0954b7fecab2d7fac4964335b558b5a3f
Sohaib Alqasem <sohaib_alqasem@yahoo.com>
2020-05-09 17:20:00 +0300

    Add include, build dir's and add [add-libraries scripts]

--- bin/proj-str.sh
+++ bin/proj-str.sh
@@ -7,12 +7,25 @@ mkdir $projName
 cd $projName
 
 mkdir Include
+mkdir build
+mkdir scripts
 
 touch CMakeLists.txt
 touch build.sh
 touch compile.sh
 chmod +x build.sh
 chmod +x compile.sh
+
+cd scripts
+touch start.sh
+touch add-libaray-static.sh
+touch add-libaray-dynamic.sh
+touch import-lib-dynamic.sh
+chmod +x start.sh
+chmod +x add-libaray-static.sh
+chmod +x add-libaray-dynamic.sh
+chmod +x import-lib-dynamic.sh
+cd ..
 
 mkdir src
 cd src
