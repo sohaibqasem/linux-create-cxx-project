97fda31e10114c15e860166a205acaeac8bcc10f
Sohaib Alqasem <sohaib_alqasem@yahoo.com>
2020-05-11 14:25:32 +0300

    update scripts folder

---
+++ bin/scripts.sh
@@ -0,0 +1,33 @@
+projName=$1
+
+
+cd scripts
+echo "
+#!/bin/sh
+cd ..
+rm -r build/
+mkdir build
+./build.sh
+./compile.sh
+" > start.sh
+
+echo "
+libName=\$1
+cd ..
+echo \"
+target_link_libraries($projName PRIVATE ${CMAKE_SOURCE_DIR}/libs/$libName.a)
+\" >> CMakeLists.txt
+" > imp-lib-static.sh
+
+
+
+echo "
+libName=\$1
+cd ..
+echo \"
+target_link_libraries($projName PRIVATE $libName)
+\" >> CMakeLists.txt
+" > imp-lib-dynamic.sh
+
+
+cd ..
