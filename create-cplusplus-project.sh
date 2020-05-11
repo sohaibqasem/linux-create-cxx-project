3407baeafc49ad38e960812a8fa11c5c67efb08c
Sohaib Alqasem <sohaib_alqasem@yahoo.com>
2020-05-11 15:57:37 +0300

    fix bug in main script

--- create-cplusplus-project.sh
+++ create-cplusplus-project.sh
@@ -4,13 +4,13 @@ projName=$1
 
 echo Creating $projName ...
 
-bin/proj-str.sh $projName
+/usr/local/bin/linux-create-cxx-project/bin/proj-str.sh $projName
 
-bin/cmake-files.sh $projName
+/usr/local/bin/linux-create-cxx-project/bin/cmake-files.sh $projName
 
-bin/scripts.sh $projName
+/usr/local/bin/linux-create-cxx-project/bin/scripts.sh $projName
 
-bin/sample.sh $projName
+/usr/local/bin/linux-create-cxx-project/bin/sample.sh $projName
 
 cd ${PWD}/$projName
 ./build.sh
